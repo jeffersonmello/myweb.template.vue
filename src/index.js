d69fcb7278bb33509759745d8f1782f65962f9f1
Jefferson <jefferson@omegainc.com.br>
2018-11-06 23:39:50 -0200

    * Added vuex

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import './assets/scss/main.scss';
 import Vue from 'vue';
+import store from '../store';
 import AppComponent from './components/AppComponent.vue';
+
 
 new Vue({
   el: '.vue-app',
-
+  store,
   components: {
     'appComponent': AppComponent
   },
  });
+
---
+++ store/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default  new Vuex.Store({
+    state: {
+      todos: [
+        { id: 1, text: '...', done: true },
+        { id: 2, text: '...', done: false }
+      ]
+    },
+    getters: {
+      doneTodos: state => {
+        return state.todos.filter(todo => todo.done)
+      }
+    }
+  });
